0fef0a460f4debfd468a81e800c597754a5aa716
MrProgrammingMan <sander.josefsson@edu.varberg.se>
2025-09-30 12:32:49 +0200

    a lot of new stuff yes

---
+++ Intro/Webbhistoria/script.js
@@ -0,0 +1,11 @@
+function showSection(sectionId, event) {
+  event.preventDefault();
+  document.querySelectorAll("section").forEach(s => s.style.display = "none");
+  const activeSection = document.getElementById(sectionId);
+  if (activeSection) activeSection.style.display = "block";
+  document.querySelectorAll("ul li a").forEach(link => link.classList.remove("active"));
+  if (event && event.currentTarget) event.currentTarget.classList.add("active");
+  window.onload = function() {
+  showSection('start', { preventDefault: () => {}, currentTarget: document.querySelector('a.active') });
+};
+}
