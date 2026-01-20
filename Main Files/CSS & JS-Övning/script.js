21069a705d4748cea6d0418e3848906b51880560
MrProgrammingMan <sander.josefsson@edu.varberg.se>
2026-01-20 12:23:07 +0100

    kubar och färgar och sånt

---
+++ Main Files/CSS & JS-Övning/script.js
@@ -0,0 +1,11 @@
+document.querySelector(".green").addEventListener("click", () => {
+  document.body.style.backgroundColor = "lightgreen";
+});
+
+document.querySelector(".red").addEventListener("click", () => {
+  document.body.style.backgroundColor = "lightpink";
+});
+
+document.querySelector(".blue").addEventListener("click", () => {
+  document.body.style.backgroundColor = "lightskyblue";
+});
