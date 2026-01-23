bfde4cfee2a25190192d98680cfba34d3ebdb8cd
MrProgrammingMan <sander.josefsson@edu.varberg.se>
2026-01-23 14:46:25 +0100

    numbers woohoo!!!

--- Main Files/CSS & JS-Övning/script.js
+++ Main Files/CSS & JS-Övning/script.js
@@ -1,5 +1,13 @@
+let greenCount = 0;
+let redCount = 0;
+let blueCount = 0;
+let totalCount = 0;
+
 document.querySelector(".green").addEventListener("click", () => {
-  if(document.body.style.backgroundColor == "lightgreen") {
+  greenCount += 1;
+  totalCount += 1;
+    document.getElementById("greenScore").textContent = greenCount;
+  if (document.body.style.backgroundColor === "lightgreen") {
     document.body.style.backgroundColor = "white"
   }
   else {
@@ -8,7 +16,10 @@ document.querySelector(".green").addEventListener("click", () => {
 });
 
 document.querySelector(".red").addEventListener("click", () => {
-  if(document.body.style.backgroundColor == "lightpink") {
+  redCount += 2;
+  totalCount += 2;
+  document.getElementById("redScore").textContent = redCount;
+  if (document.body.style.backgroundColor === "lightpink") {
     document.body.style.backgroundColor = "white"
   }
   else {
@@ -17,10 +28,22 @@ document.querySelector(".red").addEventListener("click", () => {
 });
 
 document.querySelector(".blue").addEventListener("click", () => {
-  if(document.body.style.backgroundColor == "lightblue") {
+  blueCount += 3;
+  totalCount += 3;
+    document.getElementById("blueScore").textContent = blueCount;
+  if (document.body.style.backgroundColor === "lightblue") {
     document.body.style.backgroundColor = "white"
   }
   else {
     document.body.style.backgroundColor = "lightblue";
   }
 });
+
+if (totalCount >= 15) {
+  
+}
+
+document.getElementById("greenScore").textContent = greenCount;
+document.getElementById("redScore").textContent = redCount;
+document.getElementById("blueScore").textContent = blueCount;
+
